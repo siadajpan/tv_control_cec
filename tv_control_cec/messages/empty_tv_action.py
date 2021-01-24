39da37d8fb32a88c58a3eed2359a71ab8bf3bc96
Karol Misiarz <karol.misiarz@robovision.eu>
2021-01-24 19:16:04 +0100

    Make empty message inherit after mqtt message

--- tv_control_cec/messages/empty_tv_action.py
+++ tv_control_cec/messages/empty_tv_action.py
@@ -1,11 +1,13 @@
 import time
 
+from paho.mqtt.client import MQTTMessage
+
 from tv_control_cec.messages.tv_action import TVAction
 
 
-class EmptyTVAction(TVAction):
+class EmptyTVAction(MQTTMessage):
     def __init__(self):
-        super().__init__('', '')
+        super().__init__()
 
     def execute(self):
         pass
